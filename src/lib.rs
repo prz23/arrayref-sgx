c6e185dd084f67cf9e696cbaf7e1ef710db0c4e5
prz23 <panruizheng@zju.edu.cn>
2021-07-12 11:57:27 +0800

    support sgx

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,15 @@
 #![deny(warnings)]
 #![no_std]
 
+#![cfg_attr(feature = "with-bench", feature(test))]
+
+#![cfg_attr(all(feature = "mesalock_sgx", not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
 #[cfg(test)]
 #[macro_use]
 extern crate std;
